a94d19f507361f05c86bd4dd9693c79194a9dbc5
Daan Jacobs <daan@videotechniek.nl>
2024-11-26 12:24:45 +0100

    fixed page.svelte conflicts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     import Schedule from "../lib/molecules/schedule.svelte";
     import WeekSelectie from '../lib/molecules/WeekSelectie.svelte';
-  
-    export let data;
 </script>
+<div class="skiplink">
+    <a href="#weekSelectie">Naar kalender</a>
+    <a href="#schedule">Naar schema</a>
+</div>
 
 <main>
     <WeekSelectie/>
-    <Schedule {data}/>
+    <Schedule />
 </main>
