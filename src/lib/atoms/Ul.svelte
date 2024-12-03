11599faf1c42f10064aa5c849ef825a169cbbbca
Daan Jacobs <daan@videotechniek.nl>
2024-12-03 14:35:27 +0100

    Fixes: made a ul component for the about page, closes #118

---
+++ src/lib/atoms/Ul.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Button from "./Button.svelte";
+  
+    export let numberOfLi = 0;
+    export let text = [ ];
+    export let textColor = "var(--dark)";
+    export let textSize = "var(--text-size)";
+    export let textDecoration = "none";
+    export let bold = "";
+    export let styleType = "none";
+    export let href = [ ];
+    export let label = [ ];
+  </script>
+  
+  <ul>
+    {#each Array(numberOfLi) as _, index}
+      <li style="list-style-type: {styleType};">
+        <a href={href[index] || "#"} aria-label={label[index]}>
+          <p style="color: {textColor}; font-size: {textSize}; text-decoration: {textDecoration}; font-weight: {bold};">{text[index]}</p>
+        </a>
+      </li>
+    {/each}
+  </ul>
+  
+  <style>
+    /* Voeg je eigen styling hier toe */
+  a {
+    cursor: pointer;
+  }
+  </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     // Component imports
-import Heading2 from "../../lib/atoms/Span.svelte";
+import Span from "../../lib/atoms/Span.svelte";
+import Ul from "../../lib/atoms/Ul.svelte";
 </script>
 
 <main>
-<span text="Slam" textSize="10em" textColor="var(--secondary-color)"/>
+<Span text="Slam" textSize="10em" textColor="var(--secondary-color)"/>
+<Ul numberOfLi= {3} href={["https://www.facebook.com/slamfm/?locale=nl_NL", "https://www.instagram.com/slamofficial/?hl=nl", "https://x.com/i/flow/login?redirect_after_login=/slamofficial"]} text= {["FACEBOOK", "INSTAGRAM", "X"]} textSize= "1.5em" textColor="var(--secondary-color)" label= {["FACEBOOK", "INSTAGRAM", "X"]}  textDecoration= "underline" bold= "bold"/>
 </main>
 
 <style>
