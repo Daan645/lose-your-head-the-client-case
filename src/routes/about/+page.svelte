5e3328f312b0bb77ca3935953f041f457dc574e3
Daan Jacobs <daan@videotechniek.nl>
2024-12-04 13:07:31 +0100

    Fixes: added a slide in animation on load in the slam fm section, closes #121

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,7 +8,7 @@ import Ul from "../../lib/atoms/Ul.svelte";
     <section class="slam-fm-container">
         <Span text="SLAM" textSize="13em" textColor="var(--secondary-color)"/>
         <Span text="FM" textSize="13em" textColor="var(--secondary-color)"/>
-        <Ul title= true; titleText="SOCIALS" titleSize="5em" titleColor="var(--secondary-color)" numberOfLi= {3} href={["https://www.facebook.com/slamfm/?locale=nl_NL", "https://www.instagram.com/slamofficial/?hl=nl", "https://x.com/i/flow/login?redirect_after_login=/slamofficial"]} text= {["FACEBOOK", "INSTAGRAM", "X"]} textSize= "4em" textColor="var(--secondary-color)" label= {["FACEBOOK", "INSTAGRAM", "X"]}  textDecoration= "underline" bold= "bold"/>
+        <Ul title= true; titleText="SOCIALS" titleSize="5.5em" titleColor="var(--secondary-color)" numberOfLi= {3} href={["https://www.facebook.com/slamfm/?locale=nl_NL", "https://www.instagram.com/slamofficial/?hl=nl", "https://x.com/i/flow/login?redirect_after_login=/slamofficial"]} text= {["FACEBOOK", "INSTAGRAM", "X"]} textSize= "4em" textColor="var(--secondary-color)" label= {["FACEBOOK", "INSTAGRAM", "X"]}  textDecoration= "underline" bold= "bold"/>
     </section>
 </main>
 
@@ -22,27 +22,53 @@ import Ul from "../../lib/atoms/Ul.svelte";
     position: absolute;
 }
 
-:global(.span-component:nth-of-type(2)) {
-bottom: 0;
-left: 5%;
-}
 
 :global(.span-component:first-of-type) {
+    animation: slam-fm-slide-in-right 1.5s ease-out forwards;
     top: 1;
     right: 5%;
 }
 
 :global(.span-component:nth-of-type(2)) {
-    position: absolute;
-
-bottom: 0;
-left: 5%;
+    animation: slam-fm-slide-in-left 1.5s ease-in forwards;    
+    bottom: 0;
+    left: 5%;
 }
 
 :global(.ul-component){
-bottom: 10%;
-right: 5%;
-
+    animation: socials-slide-up 1.6s ease-in forwards;
+    bottom: 10%;
+    right: 5%;
 }
 
+
+@keyframes slam-fm-slide-in-right {
+    from {
+        right: 101%;
+    }
+
+    to {
+        right: 5%;
+    }
+}
+
+@keyframes slam-fm-slide-in-left {
+    from {
+        left: 101%;
+    }
+
+    to {
+        left: 5%;
+    }
+}
+
+@keyframes socials-slide-up {
+    from {
+        bottom: -60%;
+    }
+
+    to{ 
+        bottom: 10%;
+    }
+}
 </style>
