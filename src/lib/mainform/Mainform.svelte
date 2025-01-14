f90c512866fcad15e1cce228be9fcf4ce2b895a2
WhackyTomD <deeterinktom@gmail.com>
2025-01-14 10:57:06 +0100

    Commit 1
    In this commit i rewrote the js. and deleted unused code.
    
    also i made sure the form works without js.
    
    #179

--- src/lib/mainform/Mainform.svelte
+++ src/lib/mainform/Mainform.svelte
@@ -1,12 +1,9 @@
 <script>
-    import { onMount } from "svelte";
-
     let buttonText = "Verstuur"; // Normale button text
     let isSubmitting = false; // check of de form aan het submitten is
     let message = ""; //verbind met textarea valleu
 
     async function send(event) {
-        event.preventDefault(); 
         // Zorg ervoor dat textveld eerst gevuld is
         if (!message.trim()) {
             alert("Het tekstveld mag niet leeg zijn.");
@@ -14,41 +11,34 @@
         }
 
         isSubmitting = true; 
+
         buttonText = "Bezig met versturen..."; // Update de button tekst
 
         const data = {
-            message: message,
-            date: new Date().toISOString(), // Current date and time in ISO format
+            message: message, // geschreven bericht
+            date: new Date().toISOString(),  // datum
         };
 
         try {
-            const response = await fetch("https://fdnd-agency.directus.app/items/mh_logs", {
+            await fetch("https://fdnd-agency.directus.app/items/mh_logs", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(data),
+                body: JSON.stringify(data), 
             });
-
-            if (!response.ok) {
-                throw new Error(`Server error: ${response.status}`);
-            }
-
-            // Assuming the response is successful
             buttonText = "Verstuurd";
         } catch (error) {
-            console.error("Er is een fout opgetreden", error);
             buttonText = "Er is een fout opgetreden";
-        } finally {
-            isSubmitting = false;
         }
+        event.preventDefault(); // prevent default aan het einde
     }
 </script>
 
 
 
 <div>
-    <form class="error-form" on:submit={send}>
+    <form action="https://fdnd-agency.directus.app/items/mh_logs" method="POST" class="error-form" on:submit={send}>
         <label for="textfield">Wat ging er mis?</label>
         <textarea id="textfield" spellcheck="true"  placeholder="Vertel wat u deed voor deze error" bind:value={message} required></textarea>
         <button class="form-button" type="submit" aria-label="Verstuur uw bericht" disabled={isSubmitting}>{buttonText}</button>
