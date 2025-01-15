7f3afb3802f4d151a407b5c08e70d4e0b8ebdb55
Jelte (aka driezie) <officialjustdevv@gmail.com>
2025-01-15 14:21:36 +0100

    Revert "Radio gids V2.0"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script>
-  import Header from "../lib/organism/Header.svelte";
-  import Mobilenav from "../lib/organism/Mobilenav.svelte";
-  import Footer from "../lib/organism/Footer.svelte";
+    import Header from "../lib/organism/Header.svelte";
+    import Tabs from '../lib/molecules/tabs.svelte';
+    import Mobilenav from "../lib/organism/Mobilenav.svelte";
+    import Footer from '../lib/organism/Footer.svelte';
 </script>
 
-<Header />
-<Mobilenav />
+<Header/>
+<Tabs />
+<Mobilenav/>
 <slot></slot>
+<Footer/>  
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,11 @@
 <script>
-  import Tabs from "../lib/molecules/tabs.svelte";
-  import Schedule from "../lib/molecules/schedule.svelte";
-  import WeekSelectie from "../lib/molecules/WeekSelectie.svelte";
-  import Footer from "$lib/organism/Footer.svelte";
-  import Radioplayer from "../lib/organism/Radioplayer.svelte";
-
-  export let data;
+    import Schedule from "../lib/molecules/schedule.svelte";
+    import WeekSelectie from '../lib/molecules/WeekSelectie.svelte';
+  
+    export let data;
 </script>
 
 <main>
-  <!-- <Tabs /> -->
-  <WeekSelectie class="weekselectie"></WeekSelectie>
-  <Schedule class="schedule" {data}></Schedule>
-  <Radioplayer class="radioplayer"></Radioplayer>
-  <Footer class="footer"></Footer>
+    <WeekSelectie/>
+    <Schedule {data}/>
 </main>
-
-<style>
-  :global(section:has(.navigation-buttons)) {
-    grid-area: weekselectie;
-  }
-
-  :global(.schedule) {
-    grid-area: schedule;
-  }
-  :global(article.audio) {
-    grid-area: radioplayer;
-  }
-  :global(footer) {
-    grid-area: footer;
-  }
-
-  @media only screen and (min-width: 982px) {
-    main {
-      display: grid;
-      grid-template-columns: 300px 100%;
-      grid-template-rows: auto;
-      grid-template-areas:
-        "radioplayer weekselectie"
-        "radioplayer schedule"
-        "radioplayer footer";
-      overflow-x: hidden;
-    }
-  }
-</style>
